8aa893e239f60658af47cd5d1da6def266af55cd
arturkvasuk <arturkvasuk@gmail.com>
2026-07-26 12:54:16 +0000

    add env variables to main.go

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package main
 
-import "github.com/ArturSkrin/kbot/cmd"
+import (
+	"log"
+	"os"
 
+	"github.com/ArturSkrin/kbot/cmd"
+)
+
+// TELE_TOKEN берётся из переменной окружения / .env
+// Обработчики используют telebot.Context для ответа на сообщения
 func main() {
+	if os.Getenv("TELE_TOKEN") == "" {
+		log.Println("TELE_TOKEN не задан в окружении")
+	}
 	cmd.Execute()
 }
